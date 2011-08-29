2043620025f3e6aa2c39cd65890ce557475c03b0
mrwilson <csuiav@warwick.ac.uk>
2011-08-29 19:47:32 +0100

    Got the right idea, moving forward

--- src/beanstalk.lua
+++ src/beanstalk.lua
@@ -1,10 +1,13 @@
 local print = print
 local setmetatable = setmetatable
 local require = require
+local beanjob = require("src.beanjob")
+local string = string
 
 module("beanstalk")
 
 beanstalk = {}
+local crlf = "\r\n"
 
 function beanstalk:new() 
 	local object = { watching = {} }
@@ -20,13 +23,47 @@ function beanstalk:connect(server, port)
 	else
 		print("Bad connection: "..err)
 	end
-	repeat
-		line = conn:receive( "*l")
-		print( line )
-		parseLine( line )
-	until line == ""
 end
 
-function beanstalk:listen(tube)
+function beanstalk:put(job, priority)
+	if self.connection ~= nil then
+		repeat
+			line = self.connection:receive( "*l")
+			
+		until line == ""
+	end
+end
+
+function beanstalk:reserve()
+	if self.connection ~= nil then
+		repeat
+			line = self.connection:receive( "*l")
+		until line == ""
+	else 
+		print ("No connection detected.")
+	end
+end
+
+function beanstalk:use(tube) 
+	print("use - Trying to use: "..tube)
+	if self.connection ~= nil then
+		self.connection:send("use "..tube..crlf)
+		repeat
+			line = self.connection:receive( "*l")
+			if (starts(line,"USING")) then
+				print("use - Successfully using "..tube)
+				return nil
+			else 
+				return line
+			end
+		until line == ""
+	else 
+		print ("No connection detected.")
+	end
+end
+
+function starts(String,Start)
+   return string.sub(String,1,string.len(Start))==Start
+end
 
 return beanstalk 
